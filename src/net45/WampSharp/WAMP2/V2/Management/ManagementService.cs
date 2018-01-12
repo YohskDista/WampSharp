ebcc6c4506c477e6c4331fcefc6c743461234be1
Elad Zelingher <elad@codesharp.net>
2018-01-12 11:26:58 +0200

    Very initial management work

---
+++ src/net45/WampSharp/WAMP2/V2/Management/ManagementService.cs
@@ -0,0 +1,21 @@
+﻿using WampSharp.V2.Rpc;
+
+namespace WampSharp.V2.Management
+{
+    public interface IManagementService
+    {
+        [WampProcedure("wamp.session.kill")]
+        void KillSession(long sessionId, string reason, string message);
+
+        [WampProcedure("wamp.registration.remove_callee")]
+        void RemoveCallee(long registrationId, long calleeId, string reason);
+
+        [WampProcedure("wamp.subscription.remove_subscriber")]
+        void RemoveSubscriber(long subscriptionId, long subscriberId, string reason);
+    }
+
+    public class ManagementService
+    {
+        
+    }
+}
